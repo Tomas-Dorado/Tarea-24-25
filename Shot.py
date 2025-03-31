3f5250e7554ac2ff52f2b9ef1399770ae2dfd025
Tomas-Dorado <tdoradog@student.universidadunie.com>
2025-03-31 21:35:30 +0000

    Creado shot

---
+++ Shot.py
@@ -0,0 +1,10 @@
+from Entity import Entity
+
+class Shot(Entity):
+    def move(self):
+        # Logic to move the shot
+        pass
+
+    def hit_target(self, target):
+        # Logic to check if the shot hits the target
+        pass
