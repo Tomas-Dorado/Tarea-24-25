07d3602fc440321395e94146694851a7ec171ab3
Tomas-Dorado <tdoradog@student.universidadunie.com>
2025-04-01 05:45:12 +0000

    Cambios

--- Game.py
+++ Game.py
@@ -1,3 +1,6 @@
+from Opponent import Opponent
+from Player import Player
+
 class Game:
     def __init__(self):
         self.score = 0
@@ -20,3 +23,13 @@ class Game:
     def end_game(self):
         self.is_running = False
         print("Game ended!")
+
+    def convert_enemy_to_star(self):
+        if self.is_running and self.opponent:
+            self.score += 1
+            print(f"{self.opponent} converted to star! Current score: {self.score}")
+            self.opponent = None  # Remove the opponent after conversion
+        elif not self.is_running:
+            print("Game is not running. Cannot convert enemy.")
+        else:
+            print("No opponent to convert.")
